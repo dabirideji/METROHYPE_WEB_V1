caed19523a3d32dcf9e20b81e5f5d3256feeffa6
Akinwaleadeola <olaami2705@gmail.com>
2024-12-20 10:48:10 +0100

    removed the html for the loader and edited the js file and also changed the  js file

--- WEB_ROOT/JS/LOADERINDICATOR/LoaderIndicator.js
+++ WEB_ROOT/JS/LOADERINDICATOR/LoaderIndicator.js
@@ -1,19 +1,36 @@
-const showLoader = () => {
-    document.getElementById('page-loader').classList.remove('hidden');
-};
-  
-const hideLoader = () => {
-    document.getElementById('page-loader').classList.add('hidden');
-};  
-
-
-// inline button spinner : this is for the button 
-
-function Buttonspinner(button) {
-    const spinner = document.createElement('span');
-    spinner.classList.add('spinner');
-
-    button.appendChild(spinner); 
-    button.disabled = true; 
-    spinner.style.display = 'inline-block'; 
+function createLoader() {
+    const loader = document.createElement('div');
+    loader.id = 'page-loader';
+    loader.className = 'page-loader hidden';
+    
+    const spinner = document.createElement('div');
+    spinner.className = 'spinner';
+    
+    loader.appendChild(spinner);
+    document.body.appendChild(loader);
 }
+
+function showLoader() {
+    const loader = document.getElementById('page-loader');
+    if (loader) {
+        loader.classList.remove('hidden');
+    }
+}
+
+function hideLoader() {
+    const loader = document.getElementById('page-loader');
+    if (loader) {
+        loader.classList.add('hidden');
+    }
+}
+
+//inline button spinner: this is for the button
+function buttonSpinner(button) {
+    const spinner = document.createElement('span');
+    spinner.classList.add('buttonspinner');
+    button.appendChild(spinner);
+    button.disabled = true;
+    spinner.style.display = 'inline-block';
+}
+
+createLoader();
